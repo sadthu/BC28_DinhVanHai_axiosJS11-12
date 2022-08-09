735f9d1c1283e4be7b3d9710eb6f081da300deb0
sadthu <dinhhai4443211@gmail.com>
2022-08-09 23:10:52 +0700

    add

---
+++ control/index.js
@@ -0,0 +1,118 @@
+function getProduct() {
+    var promise = axios({
+        url: 'http://svcy.myclass.vn/api/Product/GetAll',
+        method: 'GET'
+    });
+
+    promise.then(function(result) {
+        renderProduct(result.data);
+    });
+
+    promise.catch(function(err) {
+        console.log('result',err.response.data);
+    });
+}
+
+window.onload = function () {
+    getProduct();
+}
+
+function renderProduct(arrProduct) {
+    var html = '';
+    for (var i = 0; i < arrProduct.length; i++) {
+        var product = arrProduct[i];
+        html += `
+        <tr>
+            <td>${product.id}</td>
+            <td><img src="${product.img}" alt="" style="width:50px"></td>
+            <td>${product.name}</td>
+            <td>${product.price}</td>
+            <td>${product.description}</td>
+            <td>${product.type}</td>
+            <td>
+                <button class="btn btn-primary mr-2" onclick="suaProduct('${product.id}')"><i class="far fa-edit"></i></button>
+                <button class="btn btn-danger" onclick="xoaProduct('${product.id}')"><i class="far fa-trash-alt"></i></button>
+            </td>
+        </tr>
+        `
+    }
+    document.querySelector('#tbProduct').innerHTML = html;
+}
+
+document.querySelector('#btnCreate').onclick = function () {
+    var product = new Product();
+    product.id = document.querySelector('#id').value;
+    product.name = document.querySelector('#name').value;
+    product.price = document.querySelector('#price').value;
+    product.img = document.querySelector('#img').value;
+    product.description = document.querySelector('#description').value;
+    product.type = document.querySelector('#type').value;
+    var promise = axios({
+        url: 'http://svcy.myclass.vn/api/Product/CreateProduct',
+        method: 'POST',
+        data: product
+    });
+    promise.then(function (result) {
+        getProduct();
+    })
+    promise.catch(function (err) {
+        console.log(err)
+    })
+}
+
+function xoaProduct(id) {
+    var promise = axios({
+        url:'http://svcy.myclass.vn/api/Product/DeleteProduct/' + id,
+        method:'DELETE'
+    });
+
+    promise.then(function(result) {
+        getProduct();
+    });
+
+    //Xử lý thất bại
+    promise.catch(function(error) {
+    })
+
+}
+
+function suaProduct(id) {
+    var promise = axios ({
+        url: 'http://svcy.myclass.vn/api/Product/GetById/' + id,
+        method: 'GET'
+    })
+    promise.then(function(result) {
+        var inforProduct = result.data;
+        document.querySelector('#id').value = inforProduct.id;
+        document.querySelector('#name').value = inforProduct.name;
+        document.querySelector('#price').value = inforProduct.price;
+        document.querySelector('#img').value = inforProduct.img;
+        document.querySelector('#description').value = inforProduct.description;
+        document.querySelector('#type').value = inforProduct.type;
+    })
+    promise.catch(function(err) {
+
+    })
+}
+
+document.querySelector('#btnUpdate').onclick = function () {
+    var product = new Product();
+    product.id = document.querySelector('#id').value;
+    product.name = document.querySelector('#name').value;
+    product.price = document.querySelector('#price').value;
+    product.img = document.querySelector('#img').value;
+    product.description = document.querySelector('#description').value;
+    product.type = document.querySelector('#type').value;
+    var promise = axios({
+        url: 'http://svcy.myclass.vn/api/Product/UpdateProduct/' + product.id,
+        method: 'PUT',
+        data: product
+    });
+    promise.then(function (result) {
+        getProduct();
+    })
+    promise.catch(function (err) {
+        console.log(err)
+    })
+}
+
